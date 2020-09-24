9680664f78f86c607b01e56d30f009d56714fdf1
TENTACLE <tentacle@kali.TENTACLES>
2020-09-24 13:30:18 -0400

    resizing1

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -7,7 +7,7 @@
         <form action="{{route('upload')}}" method="post" class="form-group upload p-2" enctype="multipart/form-data">
             @csrf
             <input name="file" class="form-controll" type="file">
-            <button type="submit" class="btn btn-secondary">Upload</button>
+            <button type="submit" class="upload-rs btn btn-secondary">Upload</button>
         </form>
     </div>
     <div class="d-flex justify-content-center mt-2">
